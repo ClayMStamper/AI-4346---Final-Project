41d4f31136fbaaf9ab6a1bd8d36186b5c40e4f40
clayton <claystamper@gmail.com>
2019-11-25 09:03:01 -0600

    writing ai results to file

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -5,6 +5,8 @@
 #include "Hanoi.h"
 #include "AStar.h"
 
+#include "fstream"
+
 Hanoi::Hanoi(int pegCount, int diskCount) {
 
     currentNode = Node();
@@ -14,6 +16,15 @@ Hanoi::Hanoi(int pegCount, int diskCount) {
     InitializeGoal(pegCount, diskCount);
 
     Ai brain = Ai(diskCount, currentNode, goalNode);
+
+    ofstream file;
+    file.open("Results.csv");
+
+    file << "Algorithm,n,Heuristic,#Nodes Generated,#Nodes Expanded,Execution Time\n";
+    file << brain.resultString;
+
+    file.close();
+
    // RBFS brain1 = RBFS(diskCount, currentNode);
    // AStar brain2 = AStar(diskCount, currentNode);
 
